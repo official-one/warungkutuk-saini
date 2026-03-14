e8347843bb075dc2b00600fb57521f683342f572
official-one <kikisinau444@gmail.com>
2026-03-15 01:07:43 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Animasi sederhana saat scroll
-window.addEventListener("scroll", () => {
-  document.querySelectorAll(".card").forEach(card => {
-    const position = card.getBoundingClientRect().top;
-    if (position < window.innerHeight - 50) {
-      card.style.opacity = 1;
-    }
-  });
-});
